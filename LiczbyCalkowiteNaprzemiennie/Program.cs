7a1738b76877bbb13638e755f96d13d769521f89
Mateusz.Witulski <Mateusz.Witulski@PK5-15>
2022-05-08 12:13:14 +0200

    Dodaj pliki projektów.

---
+++ LiczbyCalkowiteNaprzemiennie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LiczbyCalkowiteNaprzemiennie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var x in LiczbyCalkowiteNaprzemiennie(10))
+            {
+                Console.Write($"{x} ");
+            }
+        }
+        public static IEnumerable<int> LiczbyCalkowiteNaprzemiennie(int n)
+        {
+            for(int i=0;i<=n;i++)
+            {
+                if(i%2==0 && i!=0)
+                    yield return -i;
+                else
+                    yield return i;
+            }
+        }
+    }
+}
